2c024eb79cef78dc5d80cf3906bdb00e7e889489
justahuman1 <saivdev@protonmail.com>
2020-06-28 18:52:13 -0700

    Updated builder for dynamic CMake generation
    Created build folder if not exists and signals incorrect build
    processes.

--- builder.sh
+++ builder.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cd ./build
-cmake ..
-make
+checkExistingFolder() {
+  if [ -d "$1" ]; then
+    :
+  else
+    mkdir $1
+    echo "Creating Build Folder.."
+  fi
+}
+
+checkExistingFolder ./build && cd ./build && cmake .. && make && exit 0
+
+# build process error
+exit 1
