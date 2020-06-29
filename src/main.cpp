eadc2820887df03045a5a068217edb17ea49c9d9
justahuman1 <saivdev@protonmail.com>
2020-06-28 18:40:15 -0700

    Added CLI Parser | Created CMAKE Builder
    The CMake builder is used to automate the build generation process. In
    the CMake, we added a binary folder for a cleaner output and added a
    dynamic link to the include folder.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <CLI11/CLI11.hpp>
 
-int main() {
+int main(int argc, char** argv) {
+    CLI::App app{"BashDB - A key value store for bash scripts"};
+
+    std::string key;
+    app.add_option("-p,--put", key, "Put a value in the corresponding key (will overwrite)", false);
+
+    CLI11_PARSE(app, argc, argv);
+    return 0;
+}
+
+int bashDB() {
     std::cout << "Welcome to bashDB" << std::endl;
     return 0;
 }
